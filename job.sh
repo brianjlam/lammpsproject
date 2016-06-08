94abf75f88775deec09f71b8aba9c0e54c33f6fa
brianjlam <blamm9@gmail.com>
2016-06-07 20:50:24 -0700

    data dump

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #PBS -j oe             # Combine output and error.
 #PBS -o output/job.log # Output file.
 #PBS -q default        # Queue name.
-#PBS -N FeLatParam     # Job name.
+#PBS -N FeSurf         # Job name.
 #PBS -l nodes=1:ppn=64 # Resources required.
 
 MPIRUN="/opt/openmpi-intel/bin/mpirun" # For Asathor.
@@ -12,7 +12,7 @@ NPROCS=$(wc -l < ${PBS_NODEFILE})
 cd $PBS_O_WORKDIR
 
 ${MPIRUN} -np ${NPROCS} lmp_mpi            \
-          -in           loopin.lmp         \
+          -in           surfin100.lmp         \
           -log          output/lammps.log  \
           -screen       none               \
           -var          RANDOM ${RANDOM} 
